93d9cc4712e5f6922e49cb990cb935c35fb7897e
Andrea Varnado <andrea.h.varnado@gmail.com>
2022-11-22 10:50:23 -0600

    <Exercise for JavaScript with html>

---
+++ js/external.js
@@ -0,0 +1,21 @@
+"use strict"
+console.log("Hello from external JavaScript")
+
+alert(`Welcome to my Website!`)
+
+let color = prompt (`What is your favorite color?`)
+alert (`Wow! ${color} is my favorite color too!`)
+
+let mermaid = prompt (`How many days did you rent The Little Mermaid for?`)
+let bear = prompt (`How many days did you rent Brother Bear for?`)
+let hercules = prompt (`How many days did you rent Hercules for?`)
+
+mermaid = parseInt(mermaid)
+bear = parseInt(bear)
+hercules = parseInt(hercules)
+
+//add mermaid, bear, and hercules together to get total days rented
+let totalDays = mermaid + bear + hercules
+let totalPrice = totalDays * 3
+
+alert (`You will pay ${totalPrice} to rent The Little Mermaid, Brother Bear, and Hercules!`)
