255b8e321652b9eab788bc46b22a04446c1a24b5
Andrea Varnado <andrea.h.varnado@gmail.com>
2022-12-14 14:17:50 -0600

    exercises

--- BlackJack/cards.js
+++ BlackJack/cards.js
@@ -6,10 +6,37 @@
  * other files should not directly access the deck.
  */
 //the card variables
-let suits = ["Spades","Hearts","Clubs","Diamonds"];
-let rank = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"];
-let fullDeck=suits*rank;
+// let suits = ["Spades","Hearts","Clubs","Diamonds"];
+// let rank = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"];
+// let fullDeck=suits*rank;
+//
+// console.log(fullDeck);
 
-console.log(fullDeck);
+//function to create a new card object for each card
+function card (value,name,suit){
+    this.value = value;
+    this.name = name;
+    this.suit = suit;
+}
 
+function deck(){
+    this.names=["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"];
+    this.suits=["Spades","Hearts","Clubs","Diamonds"];
+    let cards = [];
 
+    for (let i = 0; i < this.suits.length; i++) {
+        for (let j = 0; j < this.names.length; j++) {
+            cards.push(new card(j+1, this.names[j],this.suits[i]));
+        }
+    }
+    return cards;
+}
+
+let playerDeck = new deck();
+console.log(playerDeck);
+
+function shuffle(){
+    for (let i = 0; i < 1000; i++) {
+        Math.floor((Math.random()*deck.length));
+    }
+}
