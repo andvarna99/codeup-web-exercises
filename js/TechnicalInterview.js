14d9e92b4ac3b67365434e399dc0d8e5597bf82d
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-10 10:47:48 -0600

    forgot to push

---
+++ js/TechnicalInterview.js
@@ -0,0 +1,25 @@
+let books = [
+    {
+        title: "The Passage",
+        pageNum: 766
+    },
+    {
+        title: "Harry Potter and the Sorcerers Stone",
+        pageNum: 336
+    }
+    ]
+
+books.forEach(book => console.log(`Here's the title of this book: ${book.title}`));
+//specify calling the books array
+//passing an arrow function with a single book as the paramater
+//logging the title property of the book
+
+/*
+* TODO: Tell me a project you are passionate about.
+* -------------------------------------------------
+* fashion brand app to get brands that would work best for your specific body
+*
+*eye contact!!!
+*
+*
+* */
--- js/promises.js
+++ js/promises.js
@@ -1,25 +1,59 @@
 "use strict";
-
+(async function (){
 function gitUserCommitDate(username){
-    let url = "https://api.github.com/users/andvarna99/events/public";
+    let url = `https://api.github.com/users/${username}/events/public`;
     return fetch(url,{
         headers:{
             'Authorization': 'token' + GITHUB_API_KEY
-        }})
-        .then(function(response){
+        }
+    }).then(function(response){
             return response.json();
         }).then(function(data){
-            console.log(data[0].created_at.substring(0,10));
+            return data.filter(function(event) {
+                return event.type === "PushEvent";
+            })[0].created_at.substring(0,10);
         });
 }
-gitUserCommitDate();
+const lastCommitDate = await gitUserCommitDate("andvarna99");
+console.log(lastCommitDate);
+})();
+
+//walkthrough
+// (async function (){
+//     function getLastCommitDate(userName,apiKey){
+//         const url = `https://api.github.com/users/${userName}/events/public`
+//         return fetch(url, {
+//                 headers:{
+//                     'Authorization': `token ${apiKey}`
+//                 }
+//             }).then(function(response){
+//             return response.json();
+//         }).then(function(data){
+//             //dig through the events and find the most recent push event
+//             for (let i = 0; i < data.length; i++) {
+//                 if(data[i].type === "PushEvent"){
+//                     return data[i].created_at;
+//                 }
+//             }
+//             // return data[0].created_at; doesn't work, not consistent
+//         });
+//     }
+//     const lastCommitDate = await getLastCommitDate("madmarcos-codeup", GITHUB_API_KEY);
+//     // console.log(lastCommitDate);
+//
+// })();
+
+
 
 function wait(num){
-    return new Promise(function (resolve,reject){
+    return new Promise(function (resolve){
         setTimeout(function (){
-            resolve();
+            resolve(num);
         },num)
     });
 }
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(5000).then((resolveValue) => console.log(`You'll see this after ${resolveValue / 1000} seconds`));
+
+//debugging api calls
+//go into network section of inspect tools, dig into requests and responses
+//postman
